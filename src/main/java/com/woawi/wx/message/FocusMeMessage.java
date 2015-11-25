239c0eb99c27a9b05766a7751fbd2853a19827a5
夏迎霖 <xiayinglin@qianmi.com>
2015-11-25 22:03:38 +0800

    增加关注提醒功能

--- src/main/java/com/woawi/wx/message/FocusMeMessage.java
+++ src/main/java/com/woawi/wx/message/FocusMeMessage.java
@@ -1,6 +1,8 @@
 package com.woawi.wx.message;
 
 import com.woawi.wx.Environment;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
@@ -8,6 +10,8 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.WxMpXmlOutTextMessage;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.context.MessageSource;
 
 import java.util.Locale;
@@ -19,19 +23,26 @@ import java.util.Map;
  * @package com.woawi.wx.message
  * @date 15/11/25
  */
+@Slf4j
 public class FocusMeMessage implements WxMpMessageHandler {
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
         MessageSource source = Environment.getI18n();
         String msg = source.getMessage("message.welcome", null, Locale.getDefault());
-        WxMpXmlOutTextMessage m
-                = WxMpXmlOutMessage
-                .TEXT()
-                .content(msg)
-                .fromUser(wxMessage.getToUserName())
-                .toUser(wxMessage.getFromUserName())
-                .build();
+        String event = StringUtils.isBlank(wxMessage.getEvent()) ? StringUtils.EMPTY : wxMessage.getEvent();
+        WxMpXmlOutTextMessage m = null;
+        if (WxConsts.EVT_SUBSCRIBE.equals(event)) {
+            m
+                    = WxMpXmlOutMessage
+                    .TEXT()
+                    .content(msg)
+                    .fromUser(wxMessage.getToUserName())
+                    .toUser(wxMessage.getFromUserName())
+                    .build();
+        } else if (WxConsts.EVT_UNSUBSCRIBE.equals(event)) {
+        }
+        log.info("{} ---> {}", event, ToStringBuilder.reflectionToString(wxMessage));
         return m;
     }
 }
